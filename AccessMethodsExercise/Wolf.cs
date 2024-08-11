9a311e25d6630502a13420b0c79f8c822fa0bb08
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-11 14:37:45 +0200

    Add: new classes.

---
+++ AccessMethodsExercise/Wolf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessMethodsExercise
+{
+    class Wolf : Canine
+    {
+        public Wolf(bool belongsToPack)
+        {
+            BelongsToPack = belongsToPack;
+        }
+        public override void MakeNoise()
+        {
+            if (BelongsToPack)
+                Console.WriteLine("I'm in a pack.");
+            Console.WriteLine("Arooooo!");
+        }
+        public void HuntInPack()
+        {
+            if (BelongsToPack)
+                Console.WriteLine("I'm going hunting with my pack!");
+            else
+                Console.WriteLine("I'm not in a pack.");
+        }
+    }
+}
